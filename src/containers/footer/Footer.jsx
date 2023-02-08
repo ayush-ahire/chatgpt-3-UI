8474b4d70502bc0bd1b9dcd8c5b64c3cc8f80db4
ayush-ahire <ayushgithub8@gmail.com>
2023-02-08 09:12:28 +0530

    "chatgpt3-ui"

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => (
         <div className="gpt3__footer-links">
             <div className="gpt3__footer-links_logo">
                 <img src={gpt3Logo} alt="gpt3_logo" />
-                <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
+                <p>All Rights Reserved</p>
             </div>
             <div className="gpt3__footer-links_div">
                 <h4>Links</h4>
@@ -32,14 +32,13 @@ const Footer = () => (
             </div>
             <div className="gpt3__footer-links_div">
                 <h4>Get in touch</h4>
-                <p>Crechterwoord K12 182 DK Alknjkcb</p>
-                <p>085-132567</p>
-                <p>info@payme.net</p>
+                <p>09-34532389</p>
+                <p>ayu$contact@mail.net</p>
             </div>
         </div>
 
         <div className="gpt3__footer-copyright">
-            <p>@2021 GPT-3. All rights reserved.</p>
+            <p>@2023 GPT-3. All rights reserved.</p>
         </div>
     </div>
 );
